aa5a79260ae9226a1142ce82d82c3e950a58931a
pradyumnad <pradyumnadoddala@gmail.com>
2013-05-28 23:34:37 +0530

    TabHost delegate.
    Included TabHost delegate :)

--- src/com/pradyumna/hmt/HomeActivity.java
+++ src/com/pradyumna/hmt/HomeActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
+enum Tab {
+	INTERNAL_POOL_TAB,
+	BENCH_TAB,
+	HIRING_STATUS_TAB
+}
+
 /**
  * Created with IntelliJ IDEA. User: pradyumnad Date: 07/05/13 Time: 11:43 PM To
  * change this template use File | Settings | File Templates.
  */
-public class HomeActivity extends Activity {
-	TabHost tabHost;
+public class HomeActivity extends Activity implements TabHost.OnTabChangeListener {
+	private TabHost tabHost;
+	private Tab currentTab;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,7 @@ public class HomeActivity extends Activity {
 		setContentView(R.layout.activity_home);
 
 		tabHost = (TabHost)findViewById(R.id.tabHost);
+		tabHost.setOnTabChangedListener(this);
 		tabHost.setup();
 
 		TabSpec spec1 = tabHost.newTabSpec("Internal Pool");
@@ -36,5 +44,40 @@ public class HomeActivity extends Activity {
 		tabHost.addTab(spec1);
 		tabHost.addTab(spec2);
 		tabHost.addTab(spec3);
+	}
+
+	@Override
+	public void onTabChanged(String tabId) {
+		if (tabId.equals("Internal Pool")) {
+			currentTab = Tab.INTERNAL_POOL_TAB;
+		} else if (tabId.equals("Bench")) {
+			currentTab = Tab.BENCH_TAB;
+		} else if (tabId.equals("Hiring Status")) {
+			currentTab = Tab.HIRING_STATUS_TAB;
+		}
+
+		updateTabContent(currentTab);
+	}
+
+	/**
+	 *
+	 * @param tab
+	 *
+	 * @description Used to update content when tapped on a Tab
+	 */
+	public void updateTabContent(Tab tab) {
+		switch (tab) {
+			case INTERNAL_POOL_TAB:
+
+				break;
+
+			case BENCH_TAB:
+
+			 	break;
+
+			case HIRING_STATUS_TAB:
+
+				break;
+		}
 	}
 }
