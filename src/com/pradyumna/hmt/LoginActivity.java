7ee0d85ea1444186a073253982e390a5c12819a9
pradyumnad <pradyumnadoddala@gmail.com>
2013-05-30 11:02:58 +0530

    Login Activity crash..

--- src/com/pradyumna/hmt/LoginActivity.java
+++ src/com/pradyumna/hmt/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.pradyumna.hmt;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,19 +8,12 @@ import android.content.DialogInterface;
 import helpers.WSHelper;
 import helpers.WSListener;
 import helpers.WSType;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -31,7 +21,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends Activity  {
 
